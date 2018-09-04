b01d7aabc381660acc1d0c9d949f21330837e8e8
Kevin Groß <business.kevin.gross@gmai.com>
2018-09-04 20:21:39 +0200

    Adds creating a character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,23 @@
 class CharactersController < ApplicationController
   def index
   end
+
+  def new
+  	@character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to @character
+    #else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+     # render "new"
+    end
+  end
+
+  def character_params
+    params.require(:character).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post 'characters/create'
+
+  get 'characters/new'
+
   get 'characters/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
