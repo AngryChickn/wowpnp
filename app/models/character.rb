57cce7e188c609090829f1aa040f876bdaf36953
kevin groß <kg@naymspace.de>
2018-09-05 23:15:13 +0200

    fixes the race_mod and a typo

--- app/models/character.rb
+++ app/models/character.rb
@@ -9,30 +9,31 @@ class Character < ApplicationRecord
   def initMods
     klass_mod = getInitKlassMod
     race_mod = getInitRaceMod
+
     self.hitpoints = 10 + (klass_mod[:hitpoints] || 0) + (race_mod[:hitpoints] || 0)
-    self.stamina = 10 + (klass_mod[:stamina] || 0) + (race_mod[:hitpoints] || 0)
-    self.mana = 0 + (klass_mod[:mana] || 0) + (race_mod[:hitpoints] || 0)
+    self.stamina = 10 + (klass_mod[:stamina] || 0) + (race_mod[:stamina] || 0)
+    self.mana = 0 + (klass_mod[:mana] || 0) + (race_mod[:mana] || 0)
     self.armor = 0
     self.resistence = 0 + (klass_mod[:resistence] || 0) + (race_mod[:resistence] || 0)
 
-    self.strength = 1 + (klass_mod[:strength] || 0) + (race_mod[:hitpoints] || 0)
-    self.agility = 1 + (klass_mod[:agility] || 0) + (race_mod[:hitpoints] || 0)
-    self.intellect = 1 + (klass_mod[:intellect] || 0) + (race_mod[:hitpoints] || 0)
-    self.cunning = 1 + (klass_mod[:cunning] || 0) + (race_mod[:hitpoints] || 0)
-    self.willpower = 1 + (klass_mod[:willpower] || 0) + (race_mod[:hitpoints] || 0)
-    self.presence = 1 + (klass_mod[:presence] || 0) + (race_mod[:hitpoints] || 0)
+    self.strength = 1 + (klass_mod[:strength] || 0) + (race_mod[:strength] || 0)
+    self.agility = 1 + (klass_mod[:agility] || 0) + (race_mod[:agility] || 0)
+    self.intellect = 1 + (klass_mod[:intellect] || 0) + (race_mod[:intellect] || 0)
+    self.cunning = 1 + (klass_mod[:cunning] || 0) + (race_mod[:cunning] || 0)
+    self.willpower = 1 + (klass_mod[:willpower] || 0) + (race_mod[:willpower] || 0)
+    self.presence = 1 + (klass_mod[:presence] || 0) + (race_mod[:presence] || 0)
   end
 
   # returns a hash of modifiers for an attribute
   # should return also Skills later
   # TODO: Documentate balancing for this mod
-  # Mana classes get 10 Mana for free
+  # Mana classes get 10 Mana for free (like stamina)
   # 10 points total
   def getInitKlassMod
     # TODO: Refactor
     case klass.name
     when 'Krieger'
-      return { hitpoints: 5, stamina: 3, strengh: 1, agility: 1 }
+      return { hitpoints: 5, stamina: 3, strength: 1, agility: 1 }
     when 'Paladin'
       return { hitpoints: 3, stamina: 3, mana: 12, strength: 1, presence: 1 }
     when 'Jäger'
