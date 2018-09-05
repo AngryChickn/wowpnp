7b72ae0968c1498cec0c24fb690485f115134d91
kevin groß <kg@naymspace.de>
2018-09-05 22:00:57 +0200

    Adds klass mods when creating a character
    Has bad mapping tho :shrug:

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,20 +9,21 @@ class CharactersController < ApplicationController
   def create
     @character = Character.new(character_params)
     # TODO: Move to Character-class
+    klass_mod = @character.getInitKlassMod     
     @character.level = 1
 
-    @character.hitpoints = 10
-    @character.stamina = 10
-    @character.mana = 10
+    @character.hitpoints = 10 + (klass_mod[:hitpoints] || 0)
+    @character.stamina = 10 + (klass_mod[:stamina] || 0)
+    @character.mana = 10 + (klass_mod[:mana] || 0)
     @character.armor = 0
     @character.resistence = 0
 
-    @character.strength = 1
-    @character.agility = 1
-    @character.intellect = 1
-    @character.cunning = 1
-    @character.willpower = 1
-    @character.presence = 1
+    @character.strength = 1 + (klass_mod[:strength] || 0)
+    @character.agility = 1 + (klass_mod[:agility] || 0)
+    @character.intellect = 1 + (klass_mod[:intellect] || 0)
+    @character.cunning = 1 + (klass_mod[:cunning] || 0)
+    @character.willpower = 1 + (klass_mod[:willpower] || 0)
+    @character.presence = 1 + (klass_mod[:presence] || 0)
 
     if @character.save
       redirect_to @character
--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,27 @@ class Character < ApplicationRecord
   validates :level, :strength, :agility, :intellect, :cunning, :willpower, :presence,
             :hitpoints, :stamina, :mana, :armor, :resistence, presence: true
 
+  def getInitKlassMod
+    # TODO: better mapping
+    case klass.name
+    when 'Krieger'
+      return { hitpoints: 5, stamina: 3, strengh: 1 }
+    when 'Paladin'
+      return { hitpoints: 3, stamina: 3, mana: 3, presence: 1 }
+    when 'Jäger'
+      return { hitpoints: 2, stamina: 5, mana: 2, agility: 1 }
+    when 'Schamane'
+      return { hitpoints: 2, stamina: 3, mana: 4, intellect: 1 }
+    when 'Druide'
+      return { hitpoints: 3, stamina: 3, mana: 3, intellect: 1 }
+    when 'Schurke'
+      return { hitpoints: 2, stamina: 5, agility: 1, cunning: 1 }
+    when 'Hexenmeister'
+      return { hitpoints: 2, mana: 5, intellect: 1, cunning: 1 }
+    when 'Magier'
+      return { hitpoints: 0, mana: 7, intellect: 1, willpower: 1 }
+    when 'Priester'
+      return { hitpoints: 1, mana: 6, willpower: 1, presence: 1 }
+    end
+  end
 end
