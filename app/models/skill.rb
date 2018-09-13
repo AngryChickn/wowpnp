096bae1e28263254fc1b1766939699e630f98592
kevin groß <kg@naymspace.de>
2018-09-13 09:01:31 +0200

    Adds :passive skill_type

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,7 +3,7 @@ class Skill < ApplicationRecord
   belongs_to :klass, optional: true
 
   enum bound_attribute: [:strength, :agility, :intellect, :cunning, :willpower, :presence]
-  enum skill_type: [:common, :weapon, :spell, :ultimate, :profession]
+  enum skill_type: [:common, :weapon, :spell, :ultimate, :passive, :profession]
 
   validates :name, :description, :bound_attribute, :skill_type, presence: true
 end
