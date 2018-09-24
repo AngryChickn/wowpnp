94f23cff88508b83289d57b6ece81b9b634b2f08
kevin groß <kg@naymspace.de>
2018-09-24 09:49:05 +0200

    Adds validation to the skill level

--- app/models/characters_skills.rb
+++ app/models/characters_skills.rb
@@ -1,5 +1,8 @@
 class CharactersSkills < ApplicationRecord
   belongs_to :skill
   belongs_to :character
-  validates_presence_of :level
+  validates :level, numericality: {
+    only_integer: true,
+    less_than_or_equal_to: 5,
+  }
 end
